457e7aff39c020e9884685c8f19ffdc81bf83f9c
FG98 <fg98ff@gmail.com>
2026-04-14 11:37:29 -0300

    fix

--- plugins/owner-resetUser.js
+++ plugins/owner-resetUser.js
@@ -1,39 +1,34 @@
+let handler = async (m, { conn }) => {
 
-let handler = async (m, { conn, text }) => {
-    function no(number){
-    return number.replace(/\s/g,'').replace(/([@+-])/g,'')
+  let user
+  if (m.quoted) {
+    user = m.quoted.sender
+  } else if (m.mentionedJid && m.mentionedJid[0]) {
+    user = m.mentionedJid[0]
+  } else {
+    user = m.sender
   }
 
-    text = no(text)
-
-  if(isNaN(text)) {
-		var number = text.split`@`[1]
-  } else if(!isNaN(text)) {
-		var number = text
+  // 📌 Verificar si existe en la DB
+  if (!global.db.data.users[user]) {
+    return m.reply('❏ El usuario no está en la base de datos')
   }
 
-    if(!text && !m.quoted) return m.reply(`*❏ RESETEAR A USUARIO*\n\nEtiquete al usuario, escriba el número o responda al mensaje del usuario que desea REINICIAR`)
-    if(isNaN(number)) return m.reply(`❏ El número que ingresaste no es válido`)
+  delete global.db.data.users[user]
 
-      try { 
-		if(text) {
-			var user = number + '@s.whatsapp.net'
-		} else if(m.quoted.sender) {
-			var user = m.quoted.sender
-		} else if(m.mentionedJid) {
-  		  var user = number + '@s.whatsapp.net'
-			}  
-		} catch (e) {
-  } finally {
-    	let number = user.split('@')[0]
-        delete global.global.db.data.users[user]
-        conn.reply(m.chat, `*❏ USUARIO REINICIADO*\n\n✅ Se reinició a @${number} de la *BASE DE DATOS*`, null, { mentions: [user] })
-    }
-    
+  let number = user.split('@')[0]
+
+  conn.reply(
+    m.chat,
+    `*❏ USUARIO ELIMINADO*\n\n✅ @${number} fue eliminado de la base de datos`,
+    m,
+    { mentions: [user] }
+  )
 }
+
 handler.help = ['reset-user']
 handler.tags = ['owner']
-handler.command = ['reset-user', 'resetuser'] 
+handler.command = ['reset-user', 'resetuser']
 handler.rowner = true
 
 export default handler
